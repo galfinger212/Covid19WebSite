0ed239e327d42181516d7c286af44755acbd5efb
galfinger212 <galfinger1@gmail.com>
2021-06-17 15:22:34 +0300

    AddedselectBox

--- src/services/destinations.service.ts
+++ src/services/destinations.service.ts
@@ -10,25 +10,37 @@ import { URL } from '../models/DataEnum';
 export class DestinationsService {
   destinationConfirmationData: destinationConfirmation[] = [];
 
-  constructor(private httpClient: HttpClient) {}
-   
- 
-  private GetData(GetDataProperty: { (): destinationConfirmation[] }): Promise<destinationConfirmation[]> {
-    return new Promise((res,rej) => {
+  constructor(private httpClient: HttpClient) { }
+
+
+  private GetData(GetDataProperty: { (): any[] }): Promise<any[]> {
+    return new Promise((res, rej) => {
       this.httpClient.get<allDataDestinations>(URL.Destinations).subscribe(data => {
         this.destinationConfirmationData = data.result.records;
         res(GetDataProperty());
-      },error => rej(error))
+      }, error => rej(error))
     });
   }
-
+  GetAllcountriesEnglish(): any {
+    return new Promise((res, rej) => {
+      this.httpClient.get<any>(URL.allCountries).subscribe(data => {
+        let array = data.map(d => d.name);
+        res(array);
+      })
+    });
+  }
   filterByDestination(destination: string): Promise<destinationConfirmation[]> {
     return this.GetData(() => this.destinationConfirmationData.filter(d => d.destination.toString().includes(destination)));
   }
 
-  filterByStatus(isGreen: boolean): Promise<destinationConfirmation[]> {  
+  filterByStatus(isGreen: boolean): Promise<destinationConfirmation[]> {
     return this.GetData(() => this.getIsGreen(isGreen));
   }
+
+  GetAllcountries(): Promise<string[]> {
+    return this.GetData(() => this.destinationConfirmationData.map(d => d.destination));
+  }
+
 
   getIsGreen(isGreen: boolean): destinationConfirmation[] {
     let c = this.destinationConfirmationData.filter(d => {
